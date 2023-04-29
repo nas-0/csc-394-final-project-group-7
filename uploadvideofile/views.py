c3e7ecaa9709cd3d146eef81da69b13963935851
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-04-29 01:39:52 -0500

    Pushing the url in the system

--- uploadvideofile/views.py
+++ uploadvideofile/views.py
@@ -7,8 +7,11 @@ def index(request):
     return HttpResponse("You are at the website to upload YT video on the youtube platform")
 
 def upload(request):
+    context={}
     if request.method=='POST':
             uploaded_video_file=request.FILES["video"]
             fs=FileSystemStorage()
-            fs.save(uploaded_video_file.name,uploaded_video_file)
-    return render(request,'upload.html')
+            name=fs.save(uploaded_video_file.name,uploaded_video_file)
+            url=fs.url(name)
+            context['url']=fs.url(name)
+    return render(request,'upload.html',context)
