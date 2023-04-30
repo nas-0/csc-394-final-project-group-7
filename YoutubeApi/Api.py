5d0f032d17d89f95ff499641ea6dcf762dedf5f1
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-04-30 02:39:13 -0500

    Json files create, add settings
    -json file will be use for youtube api but needs to be update with the correct id,
    
    -settings was change to google api and add a dummy channel id

--- YoutubeApi/Api.py
+++ YoutubeApi/Api.py
@@ -6,6 +6,26 @@ import re
 import googleapiclient.discovery
 import googleapiclient.errors
 
-from main.models import Video
 
+
+class Youtube:
+
+    def __init__(self,*args,**kwargs):
+
+        # this is where we get data from youtube api
+        self.developer_key=settings.YOUTUBE_API_KEY
+        self.api_service_name = settings.YOUTUBE_API_SERVICE_NAME
+        self.api_version = settings.YOUTUBE_API_VERSION
+        self.channel_id = settings.YOUTUBE_CHANNEL_ID
+
+        self.youtube = googleapiclient.discovery.build(
+            self.api_service_name, 
+            self.api_version, 
+            developerKey=self.developer_key)
+    def get_date(self):
+       #this where  we data from youtube api
+
+
+        def schedule_api():
+            pass
 
--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,8 @@ MEDIA_URL='/video/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GOOGLE_API_KEY=('AIzaSyDO_y1mjNCt2ca26RBNkAZPlzwurR5-5Og')
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+CHANNEL_ID=('UC0FLNPPAKPntIZDdjNRdJLQ')
