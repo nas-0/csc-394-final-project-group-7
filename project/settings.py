838cc283a4249db5a748d98563a8f49c52377c16
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-01 04:55:56 -0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -137,7 +137,7 @@ API_VERSION = 'v3'
 CHANNEL_ID=('UC0FLNPPAKPntIZDdjNRdJLQ')
 
 
-GOOGLE_CLIENT_ID = '925184637596-o20botqnn8clfjik14jghstn37jd04oh.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET= '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'
+GOOGLE_CLIENT_ID = '925184637596-i8hgl4s7q7a0qqasfepfoo2vcv9dtfke.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET= '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'
 GOOGLE_REDIRECT_URI='https://mutiplatformsvideosupload.net/uploadvideofile/upload/'
 
