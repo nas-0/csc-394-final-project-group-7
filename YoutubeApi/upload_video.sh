cea4537607a36e7f913d7378e4848f2aa7a84b73
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-01 04:14:48 -0500

    Update upload_video.sh

--- YoutubeApi/upload_video.sh
+++ YoutubeApi/upload_video.sh
@@ -1,5 +1,4 @@
-python upload_video.py 
-                       --file="/home/ubuntu/hw/uploadvideofile/videosdatabase/TESTING.mp4"
+python upload_video.py --file="/home/ubuntu/hw/uploadvideofile/videosdatabase/TESTING.mp4"
                        --title="Testing"
                        --description="Had fun surfing in Santa Cruz"
                        --keywords="Testing, testing1"
