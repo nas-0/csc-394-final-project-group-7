0a1292af281c7821f625cc91c3dbe3e62180a6ba
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-01 04:15:29 -0500

    Update upload_video.sh

--- YoutubeApi/upload_video.sh
+++ YoutubeApi/upload_video.sh
@@ -1,6 +1,6 @@
 python upload_video.py --file="/home/ubuntu/hw/uploadvideofile/videosdatabase/TESTING.mp4"
-                       --title="Testing"
-                       --description="Had fun surfing in Santa Cruz"
-                       --keywords="Testing, testing1"
-                       --category="28"
-                       --privacyStatus="private"
+                       --title='Testing'
+                       --description='Had fun surfing in Santa Cruz'
+                       --keywords='Testing, testing1'
+                       --category='28'
+                       --privacyStatus='private'
