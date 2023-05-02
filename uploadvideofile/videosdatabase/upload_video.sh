d659cc1dbe4bb19d569ef48b205b2f038df40f61
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-01 19:13:39 -0500

    Update upload_video.sh

--- uploadvideofile/videosdatabase/upload_video.sh
+++ uploadvideofile/videosdatabase/upload_video.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python upload_video.py
---file="/home/ubuntu/hw/uploadvideofile/videosdatabase/TESTING2.mp4" 
---title='Testing'
---description='Had fun surfing in Santa Cruz'
---keywords='Testing, testing1'
---category='28'
---privacyStatus='private'
+python upload_video.py  \
+                        --file="/home/ubuntu/hw/uploadvideofile/videosdatabase/TESTING2.mp4" \
+                        --title='Testing' \
+                        --description='Had fun surfing in Santa Cruz' \
+                        --keywords='Testing, testing1' \
+                        --category='28' \
+                        --privacyStatus='private'
