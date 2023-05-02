2af27c146c00a9371418a5b3530eddb998ec6bfd
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-02 02:30:17 -0500

    youtube api

--- uploadvideofile/views.py
+++ uploadvideofile/views.py
@@ -5,6 +5,9 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 import requests
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
@@ -27,22 +30,53 @@ def upload(request):
         # Save the video file to the file system
         fs = FileSystemStorage()
         name = fs.save(uploaded_video_file.name, uploaded_video_file)
-        video_path2 = fs.path(name)
-        # Get the authorization code from the POST data
-        # Get an access token using the authorization code
-        video_path = "/home/ubuntu/hw/uploadvideofile/TESTING2.mp4"
-        title = "Testing"
-        description = "Had fun surfing in Santa Cruz"
-        keywords = "Testing, testing1"
-        category = "28"
-        privacy_status = "private"
-        #command = f"./upload_video.sh {video_path} '{title}' '{description}' '{keywords}' '{category}' '{privacy_status}'"
-        #command = ['/home/ubuntu/hw/uploadvideofile/upload_video.sh', video_path, title, description, keywords, category, privacy_status]
-        #command = ['./upload_video.sh', video_path, title, description, keywords, category, privacy_status]
-        command = ['sh', 'upload_video.sh', video_path, title, description, keywords, category, privacy_status]
+        video_path = fs.path(name)
 
-        subprocess.call(command)
-        output = subprocess.check_output(command, stderr=subprocess.STDOUT)
-        print(output.decode())
+        # Upload the video file to YouTube
+        video_title = request.POST.get('title')  # Get the title of the video from the form
+        video_description = request.POST.get('description')  # Get the description of the video from the form
+        video_url = upload_to_youtube(video_path, video_title, video_description)
+
+        # Add the video URL to the context dictionary
+        context['video_url'] = video_url
 
     return render(request, 'upload.html', context)
+
+def upload_to_youtube(video_path, video_title, video_description):
+    # Define the credentials for the Google service account that will be used to access the YouTube API
+    credentials = service_account.Credentials.from_service_account_file(
+        os.path.join(settings.BASE_DIR, 'path/to/service_account.json'),
+        scopes=['https://www.googleapis.com/auth/youtube.upload'])
+
+    # Create a YouTube API client object
+    youtube = build('youtube', 'v3', credentials=credentials)
+
+    try:
+        # Create a resource for the video that will be uploaded
+        video = {
+            'snippet': {
+                'title': video_title,
+                'description': video_description,
+            },
+            'status': {
+                'privacyStatus': 'public',
+            }
+        }
+
+        # Call the YouTube API to upload the video file
+        response = youtube.videos().insert(
+            part='snippet,status',
+            body=video,
+            media_body=MediaFileUpload(video_path)
+        ).execute()
+
+        # Get the URL of the uploaded video
+        video_url = f'https://www.youtube.com/watch?v={response["id"]}'
+
+        # Return the URL of the uploaded video
+        return video_url
+
+    except HttpError as error:
+        # Handle any errors that occur during the upload process
+        print(f'An error occurred: {error}')
+        return None
