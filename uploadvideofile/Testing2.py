9e1dcb66d391c385dbc8df214d2022a9fb29a7eb
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-03 04:26:29 -0500

    dff

--- uploadvideofile/Testing2.py
+++ uploadvideofile/Testing2.py
@@ -1,5 +1,5 @@
-import os
 
-cmd = 'python "D:/Github coding/csc-394-final-project-group-7/uploadvideofile/upload_video.py" --file="D:/Github coding/csc-394-final-project-group-7/uploadvideofile/TESTING2.mp4"'
+cmd = 'python "D:/Githubcoding/csc-394-final-project-group-7/uploadvideofile/upload_video.py" --file="D:/Githubcoding/csc-394-final-project-group-7/uploadvideofile/TESTING2.mp4"'
+
 os.system(cmd)
 
