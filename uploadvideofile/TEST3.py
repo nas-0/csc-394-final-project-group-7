1ccac5cb4218217086099326eaaa3d6df973c260
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-03 12:08:25 -0500

    this is final version for demo

--- uploadvideofile/TEST3.py
+++ uploadvideofile/TEST3.py
@@ -1,10 +1,3 @@
-import os
-import subprocess
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
-import requests
 from googleapiclient.http import MediaFileUpload
 
 import pickle
@@ -14,36 +7,12 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from google.auth.transport.requests import Request
 
-
-import requests
-import base64
-from time import sleep
-from uploadvideofile.models import Media
-from uploadvideofile.forms import UploadForm
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from django.conf import settings
-from django.core.files.storage import FileSystemStorage
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from googleapiclient.http import MediaFileUpload
-import httplib2
-
-
-
-
-#Define your keys from the developer portal
-CLIENT_SECRET_FILE = '/home/ubuntu/hw/uploadvideofile/client_secrets.json'
+CLIENT_SECRET_FILE = '/Githubcoding/csc-394-final-project-group-7/uploadvideofile/client_secrets.json'
 API_NAME = 'youtube'
 API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+SERVICE_ACCOUNT_FILE = '/Githubcoding/csc-394-final-project-group-7/uploadvideofile/service_accounts.json'
 
-
-
-def index(request):
-    context = {}
-    return render(request, 'index.html', context)
 
 
 def Create_Service(client_secret_file, api_name, api_version, *scopes):
@@ -79,24 +48,6 @@ def Create_Service(client_secret_file, api_name, api_version, *scopes):
         print('Unable to connect.')
         print(e)
         return None
-    
-@csrf_exempt
-def upload(request):
-    form = UploadForm(request.POST, request.FILES)
-    if request.method=='POST':
-        form = UploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            uploaded_video_file = request.FILES["video"]
-            fs = FileSystemStorage()
-            name = fs.save(uploaded_video_file.name, uploaded_video_file)
-            url = fs.url(name)
-            
-        else:
-            form = UploadForm()
-    return render(request, 'upload.html', {'form': form})
-
-
 def upload_on_youtube():
     service = Create_Service(CLIENT_SECRET_FILE,API_NAME,API_VERSION,SCOPES)
 
@@ -114,10 +65,13 @@ def upload_on_youtube():
         'notifySubscribers': False
     }
 
-    media_file = MediaFileUpload('/home/ubuntu/hw/uploadvideofile/TESTING2.mp4')
+    media_file = MediaFileUpload('/Githubcoding/csc-394-final-project-group-7/uploadvideofile./TESTING2.mp4')
 
     response_upload = service.videos().insert(
         part='snippet,status',
         body=request_body,
         media_body=media_file
     ).execute()
+
+if __name__ == "__main__":
+    upload_on_youtube()
