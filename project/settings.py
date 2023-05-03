b46afcb0cafd697f3beb86b428e9cb433de4e782
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-03 03:45:28 -0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': 'nasir123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
