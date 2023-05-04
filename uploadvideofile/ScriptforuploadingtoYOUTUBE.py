0440fed8cad3cb0f0e8be616386e6f15ee433207
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-03 19:31:46 -0500

    de

---
+++ uploadvideofile/ScriptforuploadingtoYOUTUBE.py
@@ -0,0 +1,78 @@
+from googleapiclient.http import MediaFileUpload
+
+import pickle
+import os
+from google_auth_oauthlib.flow import Flow, InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from google.auth.transport.requests import Request
+
+CLIENT_SECRET_FILE = '/Githubcoding/csc-394-final-project-group-7/uploadvideofile/client_secrets.json'
+API_NAME = 'youtube'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+SERVICE_ACCOUNT_FILE = '/Githubcoding/csc-394-final-project-group-7/uploadvideofile/service_accounts.json'
+
+
+
+def Create_Service(client_secret_file, api_name, api_version, *scopes):
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
+
+    cred = None
+
+    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
+    
+
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as token:
+            cred = pickle.load(token)
+
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            cred = flow.run_local_server()
+
+        with open(pickle_file, 'wb') as token:
+            pickle.dump(cred, token)
+
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=cred)
+        print('Service Created Successfully')
+        return service
+    except Exception as e:
+        print('Unable to connect.')
+        print(e)
+        return None
+def upload_on_youtube():
+    service = Create_Service(CLIENT_SECRET_FILE,API_NAME,API_VERSION,SCOPES)
+
+    request_body = {
+        'snippet': {
+            'categoryId': 23,
+            'title': 'THIS IS FOR TESTING',
+            'description': '',
+            'tags': []
+        },
+        'status': {
+            'privacyStatus': 'public',
+            'selfDeclaredMadeForKids': False
+        },
+        'notifySubscribers': False
+    }
+
+    media_file = MediaFileUpload('/Githubcoding/csc-394-final-project-group-7/uploadvideofile./TESTING2.mp4')
+
+    response_upload = service.videos().insert(
+        part='snippet,status',
+        body=request_body,
+        media_body=media_file
+    ).execute()
+    print("Video was uploaded successfully")
+
+if __name__ == "__main__":
+    upload_on_youtube()
