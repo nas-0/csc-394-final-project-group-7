e3bc81718b581024f8fba662757d797108623729
Jonathan jaiver Herrera <125217312+SMurf612412@users.noreply.github.com>
2023-05-08 02:28:39 -0500

    deleting unsure files

--- uploadvideofile/upload_video.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python upload_video.py  \
-                        --file="/home/ubuntu/hw/uploadvideofile/TESTING2.mp4" \
-                        --title='Testing' \
-                        --description='Had fun surfing in Santa Cruz' \
-                        --keywords='Testing, testing1' \
-                        --category='28' \
-                        --privacyStatus='private'
--- uploadvideofile/views.py
+++ uploadvideofile/views.py
@@ -1,43 +1,19 @@
 import os
 import subprocess
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
 import requests
-from googleapiclient.http import MediaFileUpload
-
 import pickle
 import os
-from google_auth_oauthlib.flow import Flow, InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
-from google.auth.transport.requests import Request
-
-
 import requests
 import base64
 from time import sleep
 from uploadvideofile.models import Media
 from uploadvideofile.forms import UploadForm
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from googleapiclient.http import MediaFileUpload
-import httplib2
 
 import praw
-
-
-#Define your keys from the developer portal
-CLIENT_SECRET_FILE = '/home/ubuntu/hw/uploadvideofile/client_secrets.json'
-API_NAME = 'youtube'
-API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
 
 
