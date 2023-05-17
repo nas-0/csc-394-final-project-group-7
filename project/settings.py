f0d528ceeeed5b9b6f369ed3b254f946d20f06c8
r4p70rj <saabj0212@gmail.com>
2023-05-17 02:24:58 -0500

    Login now redirects to index -JS

--- project/settings.py
+++ project/settings.py
@@ -147,6 +147,10 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'uploadvideofile','videosdatabase')
 
 MEDIA_URL='/video/'
 
+LOGIN_REDIRECT_URL = "/uploadvideofile"
+LOGOUT_REDIRECT_URL = "/uploadvideofile"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
